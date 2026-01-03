04a19585be5b1b29bf495a7b99029c0896c358ab
Rekha Lanjwade <rekhalanjwade@gmail.com>
2026-01-03 12:36:25 +0530

    Remove unused files

--- c.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-df = pd.read_csv("Data/phishing_cleaned_final.csv")   # Adjust the path as needed
-
-
-print("Label values before mapping:", df['Type'].unique())
-# Keep only known values
-df = df[df['Type'].isin([-1, 1])]
-df['Type'] = df['Type'].map({-1: 0, 1: 1}).astype(int)
-print("Label distribution after mapping:", df['Type'].value_counts())
